fdc6b29db5f460540d88c8cc4367a771f7f556f1
Dave Cuza <dave.cuza90@gmail.com>
2016-01-15 10:06:18 -0500

    Initial Commit

---
+++ freeling_javaAPI/freeling_javaAPIPriv.h
@@ -0,0 +1,19 @@
+/*
+ *  freeling_javaAPIPriv.h
+ *  freeling_javaAPI
+ *
+ *  Created by Dave Cuza on 15/1/16.
+ *  Copyright (c) 2016 BrightSoft. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class freeling_javaAPIPriv
+{
+	public:
+		void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
